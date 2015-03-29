806985b86ddff02d3d56768a929a4d4daa2b9a9b
1000ch <shogo.sensui@gmail.com>
2015-03-29 13:51:34 +0900

    add Brewfile & .gemrc

---
+++ Brewfile
@@ -0,0 +1,12 @@
+update
+upgrade
+
+install ansible
+install autoconf
+install automake
+install ffmpeg
+install git
+install go
+install hugo
+install pkg-config
+install zsh
--- setup.sh
+++ setup.sh
@@ -21,3 +21,4 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zprofile ~/.zprofile
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gemrc ~/.gemrc
