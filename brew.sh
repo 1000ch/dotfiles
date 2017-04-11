0fde5f1bce23a8c358b5c329388c3cb539a1a405
1000ch <shogo.sensui@gmail.com>
2017-04-11 17:56:03 +0900

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -1,23 +1,53 @@
 brew update
 brew upgrade
+brew tap caskroom/versions
 
 brew install ag
 brew install autoconf
 brew install automake
+brew install brotli
 brew install direnv
 brew install docker
 brew install docker-machine
-brew install ffmpeg
 brew install ffmpeg --with-libvpx
+brew install fish
 brew install fontforge
 brew install git
 brew install go
+brew install guetzli
 brew install hugo
 brew install highlight
 brew install imagemagick --with-webp
+brew install jq
+brew install rust
+brew install stoken
+brew install tmux
 brew install vim
 brew install wget
+brew install wine
 brew install zopfli
 brew install zsh
+
+brew cask install 1password
+brew cask install alfred
+brew cask install appcleaner
+brew cask install atom
+brew cask install bartender
+brew cask install contexts
+brew cask install dropbox
+brew cask install divvy
+brew cask install google-chrome
+brew cask install google-chrome-canary
+brew cask install google-japanese-ime
+brew cask install imagealpha
+brew cask install imageoptim
+brew cask install iterm2
+brew cask install popclip
+brew cask install sketch
+brew cask install skype
+brew cask install slack
+brew cask install spectacle
+brew cask install sublime-text
+brew cask install tower
 
 brew cleanup
