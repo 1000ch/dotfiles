4da3b1703c51e1a59aaa2d6d298a3f71719287b1
1000ch <shogo.sensui@gmail.com>
2017-12-15 17:31:35 +0900

    Remove unused bitbar plugins

--- bitbar/train-shibuya.5m.sh
+++
@@ -1,6 +0,0 @@
-# curl して HTML 取得
-curl -s http://transit.yahoo.co.jp/station/time/22715/?gid=7171 | \
-# HTML から必要な情報取り出して JSON 化
-pup 'table.tblDiaDetail [id*="hh_"] json{}' | \
-# JSON を良い感じに整形
-jq '[.[] | { hour: .children[0].text, minutes: [.children[1].children[].children[].children[].children[].children | map(.text) | join(" ") ] }]'
