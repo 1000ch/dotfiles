b94366d2e9a9f4ef91fbe17cfc7c5e60c37376b8
1000ch <shogo.sensui@gmail.com>
2018-08-06 12:32:52 +0900

    Add symlinks.sh

--- setup.sh
+++ setup.sh
@@ -21,19 +21,3 @@ git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 git clone https://github.com/tokuhirom/plenv.git ~/.plenv
 git clone https://github.com/tokuhirom/Perl-Build.git ~/.plenv/plugins/perl-build
-
-# symlinks
-ln -sf ~/workspace/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/workspace/dotfiles/.zprofile ~/.zprofile
-ln -sf ~/workspace/dotfiles/.exports ~/.exports
-ln -sf ~/workspace/dotfiles/.aliases ~/.aliases
-ln -sf ~/workspace/dotfiles/.functions ~/.functions
-ln -sf ~/workspace/dotfiles/.gemrc ~/.gemrc
-ln -sf ~/workspace/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/workspace/dotfiles/.vimrc.basic ~/.vimrc.basic
-ln -sf ~/workspace/dotfiles/.vimrc.editor ~/.vimrc.editor
-ln -sf ~/workspace/dotfiles/.vimrc.encoding ~/.vimrc.encoding
-ln -sf ~/workspace/dotfiles/.vimrc.plugins ~/.vimrc.plugins
-ln -sf ~/workspace/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/workspace/dotfiles/.editorconfig ~/.editorconfig
-ln -sf ~/workspace/dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
---
+++ symlinks.sh
@@ -0,0 +1,15 @@
+# symlinks
+ln -sf ~/workspace/github.com/1000ch/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/workspace/github.com/1000ch/dotfiles/.zprofile ~/.zprofile
+ln -sf ~/workspace/github.com/1000ch/dotfiles/.exports ~/.exports
+ln -sf ~/workspace/github.com/1000ch/dotfiles/.aliases ~/.aliases
+ln -sf ~/workspace/github.com/1000ch/dotfiles/.functions ~/.functions
+ln -sf ~/workspace/github.com/1000ch/dotfiles/.gemrc ~/.gemrc
+ln -sf ~/workspace/github.com/1000ch/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/workspace/github.com/1000ch/dotfiles/.vimrc.basic ~/.vimrc.basic
+ln -sf ~/workspace/github.com/1000ch/dotfiles/.vimrc.editor ~/.vimrc.editor
+ln -sf ~/workspace/github.com/1000ch/dotfiles/.vimrc.encoding ~/.vimrc.encoding
+ln -sf ~/workspace/github.com/1000ch/dotfiles/.vimrc.plugins ~/.vimrc.plugins
+ln -sf ~/workspace/github.com/1000ch/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/workspace/github.com/1000ch/dotfiles/.editorconfig ~/.editorconfig
+ln -sf ~/workspace/github.com/1000ch/dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
