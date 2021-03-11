bb4bcbd0ed19fc225662ede9565e2e17c1f6b7cc
1000ch <shogosensui@gmail.com>
2021-03-11 16:42:52 +0900

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -3,52 +3,36 @@ brew upgrade
 brew tap caskroom/versions
 
 brew install ag
-brew install autoconf
-brew install automake
-brew install brotli
-brew install direnv
 brew install docker
 brew install docker-machine
 brew install ffmpeg --with-libvpx
 brew install fish
-brew install fontforge
 brew install git
 brew install go
-brew install guetzli
 brew install hugo
 brew install highlight
 brew install imagemagick --with-webp
-brew install jq
-brew install peco
-brew install rust
-brew install stoken
-brew install tmux
+brew install fzf
 brew install vim
-brew install wget
-brew install wine
-brew install zopfli
 brew install zsh
 
-brew cask install 1password
-brew cask install alfred
-brew cask install appcleaner
-brew cask install atom
-brew cask install bartender
-brew cask install contexts
-brew cask install dropbox
-brew cask install divvy
-brew cask install google-chrome
-brew cask install google-chrome-canary
-brew cask install google-japanese-ime
-brew cask install imagealpha
-brew cask install imageoptim
-brew cask install iterm2
-brew cask install popclip
-brew cask install sketch
-brew cask install skype
-brew cask install slack
-brew cask install spectacle
-brew cask install sublime-text
-brew cask install tower
+brew install --cask 1password
+brew install --cask alfred
+brew install --cask appcleaner
+brew install --cask contexts
+brew install --cask dropbox
+brew install --cask divvy
+brew install --cask google-chrome
+brew install --cask google-chrome-canary
+brew install --cask google-japanese-ime
+brew install --cask imagealpha
+brew install --cask imageoptim
+brew install --cask iterm2
+brew install --cask popclip
+brew install --cask rectangle
+brew install --cask sketch
+brew install --cask skype
+brew install --cask slack
+brew install --cask visual-studio-code
 
 brew cleanup
