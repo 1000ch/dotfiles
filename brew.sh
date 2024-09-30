5faafb9533173b2053163cf44eaa5c9337763f75
1000ch <shogosensui@gmail.com>
2024-09-30 13:50:29 +0900

    Fix targets to install

--- brew.sh
+++ brew.sh
@@ -1,12 +1,9 @@
 brew update
 brew upgrade
-brew tap caskroom/versions
+brew tap homebrew/cask-versions
 
 brew install ag
-brew install docker
-brew install docker-machine
-brew install ffmpeg --with-libvpx
-brew install fish
+brew install ffmpeg
 brew install git
 brew install go
 brew install hugo
@@ -14,24 +11,19 @@ brew install highlight
 brew install imagemagick --with-webp
 brew install fzf
 brew install vim
-brew install zsh
 
 brew install --cask 1password
 brew install --cask alfred
 brew install --cask appcleaner
 brew install --cask contexts
+brew install --cask discord
 brew install --cask dropbox
-brew install --cask divvy
-brew install --cask google-chrome
-brew install --cask google-chrome-canary
+brew install --cask fork
 brew install --cask google-japanese-ime
 brew install --cask imagealpha
 brew install --cask imageoptim
-brew install --cask iterm2
 brew install --cask popclip
 brew install --cask rectangle
-brew install --cask sketch
-brew install --cask skype
 brew install --cask slack
 brew install --cask visual-studio-code
 
