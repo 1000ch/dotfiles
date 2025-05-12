2f49e4f04a0fa6b223fdc9ddd46997d0438fb54e
1000ch <shogosensui@gmail.com>
2025-05-12 15:22:24 +0900

    Add util/log.sh

---
+++ util/log.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+typeset -g -A colors
+
+colors=(
+  black 0
+  red 1
+  green 2
+  yellow 3
+  blue 4
+  magenda 5
+  cyan 6
+  white 7
+)
+
+log() {
+  key=${2:-white}
+  printf "\033[3%dm%s\033[m\n" "$colors[$key]" "$1"
+}
